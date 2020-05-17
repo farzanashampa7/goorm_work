7371242d0147c1e57fb03529ea3ea763971e692c
root <farzanashampa7@gmail.com>
2020-05-17 07:48:22 +0000

    v12

---
+++ wdb/Achilles/environment/YelpCamp/v12/app.js
@@ -0,0 +1,91 @@
+var express			= require("express"),
+ 	app				= express(),
+ 	bodyParser		= require("body-parser"),
+	mongoose		= require("mongoose"),
+	flash 			= require("connect-flash"),
+	passport		= require("passport"),
+	LocalStrategy	= require("passport-local"),
+	methodOverride	= require("method-override"),
+	Campground  	= require("./models/campground"),
+	Comment			= require("./models/comment"),
+	User 			= require("./models/user"),
+	seedDB			= require("./seeds")
+
+
+//requiring routes
+var commentRoutes 		=require("./routes/comments"),
+	campgroundRoutes 	=  require("./routes/campgrounds"),
+	indexRoutes 			= require("./routes/index")
+	
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect(process.env.DATABASEURL);
+/*mongoose.connect("mongodb+srv://farzanashampa:11112o18@cluster0-inpqh.mongodb.net/test?retryWrites=true&w=majority"),{
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log("Connected to DB");
+}).catch(err => {
+	console.log("ERROR:", err.message);
+});*/
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.use(flash());
+
+//seed the database
+//seedDB();
+
+/*Campground.create(
+	{
+		name:"Granite Hill", 
+image:"https://cdn.pixabay.com/photo/2017/06/07/12/59/poland-2380290_1280.jpg",
+		description:"This is a huge granite hill. No bathrooms, no water. Beautiful granite!"
+		
+	}, function(err, campground){
+		if(err){
+			console.log("ERROR!!");
+		}else{
+			console.log("NEWLY CREATED CAMPGROUND:")
+			console.log(campground);
+		}
+	});*/
+
+/*var campgrounds=[
+        {name:"Salmon Creek", image:"https://cdn.pixabay.com/photo/2017/08/04/20/04/camping-2581242_1280.jpg"},
+        {name:"Mountain Hill Rest", image:"https://cdn.pixabay.com/photo/2017/06/07/12/59/poland-2380290_1280.jpg"},
+        {name:"Granite Hill", image:"https://cdn.pixabay.com/photo/2016/03/30/02/57/camping-1289930_1280.jpg"}
+    ];*/
+    
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "Rusty is the cutest dog!!",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){		
+		res.locals.currentUser= req.user;
+		res.locals.error    = req.flash("error");
+		res.locals.success    = req.flash("success");
+		next();
+		});	
+
+app.use("/" ,indexRoutes);
+app.use("/campgrounds" ,campgroundRoutes);
+app.use(commentRoutes);
+
+
+app.listen(process.env.PORT, process.env.IP,function(){
+    console.log("YelpCamp is started!!");
+});
